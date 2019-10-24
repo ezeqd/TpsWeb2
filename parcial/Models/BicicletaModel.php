dd6a8bcda0ee96d1de393f00d520c80639d205cb
Ezequiel <ezequiel.domenech@gmail.com>
2019-10-24 12:44:50 -0300

    avances en el modelado de datos y conexion a la db

--- parcial/Models/BicicletaModel.php
+++ parcial/Models/BicicletaModel.php
@@ -1,1 +1,22 @@
 <?php 
+class BicicletaModel {
+    private $db;
+
+    function __construct(){
+        $this->db = new PDO('mysql:host=localhost;'.'dbname=bici_db;charset=utf8','root','');
+    }
+
+    function GetDatos($id){
+        $sentencia = $this->db->prepare("SELECT * FROM bicicleta WHERE id_bici=?");
+        $sentencia->execute(array($id));
+        $datos = $sentencia->fetchAll(PDO::FETCH_OBJ);
+        return $datos;
+    }
+
+    function GetCantidad(){
+        $sentencia = $this->db->prepare("SELECT bicicleta");
+        return $cantidad;
+    }
+}
+
+?>
